0e2b35c05fef0ea91f60ffeb2b351726f5ff2638
vikram(wsl) <vikramjit360@gmail.com>
2024-12-02 13:48:39 +0530

    13.22 - feat: add PUT /api/readinglists/:id route to change the reading property for a reading list

--- postgres-blog-app-backend/controllers/readingLists.js
+++ postgres-blog-app-backend/controllers/readingLists.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 
 const { ReadingList } = require("../models");
 
+const { extractUser } = require("../utils/middleware");
+
 const readingListsRouter = Router();
 
 readingListsRouter.post("/", async (req, res) => {
@@ -11,6 +13,22 @@ readingListsRouter.post("/", async (req, res) => {
 
   const readingList = await ReadingList.create(req.body);
   res.json(readingList);
+});
+
+readingListsRouter.put("/:id", extractUser, async (req, res) => {
+  const list = await ReadingList.findByPk(req.params.id);
+
+  if (!list) {
+    throw new Error(`reading list with id '${req.params.id}' was not found on the server`);
+  } else if (list.userId != req.user.id) {
+    throw new Error("reading list can only be change by its creator");
+  } else if (!req.body.read || typeof req.body.read != "boolean") {
+    throw new Error("the 'read' property is either missing or not boolean");
+  }
+
+  list.reading = req.body.read;
+  await list.save();
+  res.json(list);
 });
 
 module.exports = readingListsRouter;
